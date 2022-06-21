e92c5e4914b1f59a86260171081d1132f504aa24
Григорий Макаров <73779981+Rasolan@users.noreply.github.com>
2022-06-21 09:30:54 +0600

    Просто дороботки сайта от Rasolan

---
+++ js/index.js
@@ -0,0 +1,32 @@
+console.log('Book7Shelf - start site')
+let brg = document.querySelector('.burger'),
+    online = document.querySelector('.content__online span');
+brg.addEventListener('click', () =>{
+  brg.classList.toggle('burger__active')
+  document.querySelector('.nav__links').classList.toggle('nav__links-active')
+})
+fetch('https://api.mcsrvstat.us/2/book7shelf.ru').then((response) => { 
+  return response.json();
+}).then((data) =>{ 
+  if (data.online) {
+    online.innerHTML = data.players.online + " игрока из " + data.players.max + " в сети"
+  }else{
+    
+  }
+});
+
+const swiper = new Swiper('.swiper', { 
+  // Optional parameters 
+  direction: 'horizontal',
+  effect: "cards",
+  grabCursor: true,
+  loop: true, 
+  // If we need pagination 
+  pagination: { el: '.swiper-pagination', }, 
+  // Navigation arrows 
+  navigation: { nextEl: '.swiper-button-next',
+  prevEl: '.swiper-button-prev', }
+  // And if we need scrollbar 
+  
+});
+
---
+++ js/online.js
@@ -0,0 +1,8 @@
+<html> 
+    <head>
+        <title>Online</title>
+    <head/>
+    <body>
+        <h1>Online</h1>
+    </body>
+</html>
